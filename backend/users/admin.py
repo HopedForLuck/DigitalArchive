86bc2458b7fe6c915cd83e7e6e290436fd935d6e
Aleksandr Churilov <sanychurilov@gmail.com>
2024-03-15 23:07:54 +0300

    first users changes

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = (
+        'username',
+        'email',
+        'bio',
+        'first_name',
+        'last_name',
+    )
+    list_filter = (
+        'email',
+        'username',
+    )
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,62 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.db import models
+
+LENGTH_EMAIL = 256
+LENGTH_NAME = 150
+LENGTH_CODE = 36
 
 
 class User(AbstractUser):
-    pass
+    """Custom User model."""
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        'username',
+        'first_name',
+        'last_name',
+    ]
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=LENGTH_NAME,
+        blank=False,
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=LENGTH_NAME,
+        blank=False,
+    )
+    username = models.CharField(
+        max_length=LENGTH_NAME,
+        verbose_name='Логин',
+        unique=True,
+        db_index=True,
+        error_messages={
+            'unique': "Пользователь с таким логином уже существует",
+        },
+        validators=[RegexValidator(
+            regex=r'^[\w.@+-]+\Z',
+            message='Имя пользователя содержит недопустимый символ'
+        )]
+    )
+    email = models.EmailField(
+        verbose_name='Email адрес',
+        max_length=LENGTH_EMAIL,
+        unique=True,
+        error_messages={
+            'unique': "Пользователь с такой почтой уже существует",
+        },
+    )
+    bio = models.TextField(
+        blank=True,
+        verbose_name="О пользователе",
+    )
+    confirmation_code = models.CharField(
+        max_length=LENGTH_CODE,
+        null=True,
+        blank=True,
+        verbose_name="Код подтверждения",
+    )
+
+    class Meta:
+        ordering = ('id', )
